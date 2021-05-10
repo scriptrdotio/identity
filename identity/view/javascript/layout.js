eed7c2b6857b6eb5637ddfe4dc3c5f5dee770dc6
Nour Hashem <60002439+noorhachem@users.noreply.github.com>
2021-05-10 15:17:09 +0300

    fix the icon on the menu bar

--- identity/view/javascript/layout.js
+++ identity/view/javascript/layout.js
@@ -1,9 +1,7 @@
 var menuItems = {
   "mainMenu": "col1",
   "col1": [
-    {"id":"1","iconClass":"fa fa-dashboard","label":"Identity Management","route":"#/identitymanagement", "active":"true"},
-    {"id":"2", "iconClass":"fa fa-globe", "label": "Users Management", "route":"#/usermanagement", "active":"false"}
-    
+    {"id":"1","iconClass":"fa fa-user","label":"Identity Management","route":"#/identitymanagement", "active":"true"},    
   ]
 };
 var headerItems = {
@@ -15,7 +13,6 @@ var headerItems = {
 
 var routingItems = {
   "params": [
-      {"route": "usermanagement", "template": "/identity/view/templates/users.html"},
       {"route": "identitymanagement", "template": "/identity/view/html/views/identityMain.html"},
       {"route": "logout", "template": "/login/view/logout.html"}, 
   ]
